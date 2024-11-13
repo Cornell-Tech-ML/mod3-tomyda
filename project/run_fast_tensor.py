68b69ad3cef338bc14dc4a2a4c7be612051b9efc
Tomás De Angelis <tomasde.angelis17@gmail.com>
2024-11-13 14:47:51 -0500

    Test 3.5

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -29,8 +29,11 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden, 1, backend)
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        # Implementing the forward pass
+        x = self.layer1.forward(x).relu()
+        x = self.layer2.forward(x).relu()
+        x = self.layer3.forward(x).sigmoid()
+        return x
 
 
 class Linear(minitorch.Module):
@@ -43,8 +46,8 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        # Implementing the forward pass
+        return x @ self.weights.value + self.bias.value
 
 
 class FastTrain:
